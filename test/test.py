19f482a4de559b931a05a1a0080ce0586c126dab
James Xie <hx2227@nyu.edu>
2024-10-17 20:51:57 -0400

    updated /test/test.py and /docs/info.md

--- test/test.py
+++ test/test.py
@@ -4,7 +4,6 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-import random
 
 
 @cocotb.test()
@@ -17,21 +16,20 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    for i in range(1000):
-        # Set the input values you want to test
-        a = random.randint(0, 15)
-        b = random.randint(0, 15)
-        dut.a.value = a
-        dut.b.value = b
+    for a in range(16):
+        for b in range(16):
+            # Set the input values you want to test
+            dut.a.value = a
+            dut.b.value = b
 
-        # Wait for one clock cycle to see the output values
-        await ClockCycles(dut.clk, 10)
+            # Wait for one clock cycle to see the output values
+            await ClockCycles(dut.clk, 10)
 
-        # The following assersion is just an example of how to check the output values.
-        # Change it to match the actual expected output of your module:
-        dut._log.info(f"value of outputs are: {dut.sum.value} and {dut.carry_out.value}.")
-        assert dut.sum.value == (a+b)%16 and dut.carry_out.value == (a+b)//16
+            # The following assersion is just an example of how to check the output values.
+            # Change it to match the actual expected output of your module:
+            dut._log.info(f"value of outputs are: {dut.sum.value} and {dut.carry_out.value}.")
+            assert dut.sum.value == (a+b)%16 and dut.carry_out.value == (a+b)//16
 
-        # Keep testing the module by changing the input values, waiting for
-        # one or more clock cycles, and asserting the expected output values.
+            # Keep testing the module by changing the input values, waiting for
+            # one or more clock cycles, and asserting the expected output values.
     
